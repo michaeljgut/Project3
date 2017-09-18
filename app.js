e2e94a20621cb26272e5129e490873154fe2f71c
Lenora Rigoni <lenorarigoni@gmail.com>
2017-09-18 12:19:24 -0400

    linked express to react

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const app = express();
 
+app.use(express.static('client/build'))
 
-app.get('/', function(req, res) {
-    res.render('hello world');
-});
+
+// app.get('/', function(req, res) {
+//     res.render('hello world');
+// });
 
 app.listen(3000, function(){
     console.log('Example app listening on port 3000!')    
 })
+
+
